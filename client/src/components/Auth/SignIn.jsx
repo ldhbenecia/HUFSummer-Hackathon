6485e860e65f7cdb075308325626e305eb70ac47
DongHyeok Lim <jh07050@gmail.com>
2023-06-23 21:04:18 +0900

    Design: publishing

--- client/src/components/Auth/SignIn.jsx
+++ client/src/components/Auth/SignIn.jsx
@@ -1,51 +1,105 @@
-import { Box, Button, TextField, Typography, Container } from "@mui/material";
-import React from 'react'
+import React, { useState } from "react";
+import {
+  Box,
+  Button,
+  TextField,
+  Typography,
+  Container,
+  InputAdornment,
+} from "@mui/material";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import { indigo } from "@mui/material/colors";
+import { useNavigate } from "react-router-dom";
 
 const SignIn = () => {
+  const navigate = useNavigate();
+  const color = indigo[500];
+  const [id, setId] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleIdChange = (event) => {
+    setId(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    navigate("/");
+  };
+
   return (
     <>
       <Container component="main" maxWidth="xs">
         <Box
           sx={{
-            marginTop: 8,
+            marginTop: "calc(50vh - 200px)",
             display: "flex",
             flexDirection: "column",
             alignItems: "center",
           }}
         >
           <Typography component="h1" variant="h5">
-            팀명
+            LinkedDorm
           </Typography>
           <TextField
-            margin="normal"
-            label="이메일"
-            color="success"
+            id="id"
+            label="아이디"
+            name="id"
+            autoComplete="id"
             required
             fullWidth
-            name="email"
-            autoComplete="email"
-            inputProps={{ style: { fontSize: 15 } }}
-            InputLabelProps={{ style: { fontSize: 15 } }}
-            autoFocus
+            value={id}
+            onChange={handleIdChange}
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <AccountCircle />
+                </InputAdornment>
+              ),
+            }}
+            variant="standard"
           />
           <TextField
+            id="password"
             margin="normal"
             label="비밀번호"
             type="password"
-            color="success"
             required
             fullWidth
             name="password"
-            inputProps={{ style: { fontSize: 15 } }}
-            InputLabelProps={{ style: { fontSize: 15 } }}
-            autoComplete="current-password"
+            autoComplete="password"
+            value={password}
+            onChange={handlePasswordChange}
+            sx={{
+              mt: 4,
+            }}
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <AccountCircle />
+                </InputAdornment>
+              ),
+            }}
+            variant="standard"
           />
+
           <Button
             type="submit"
             fullWidth
             variant="contained"
-            color="success"
-            sx={{ mt: 3, mb: 2, fontSize: 15 }}
+            sx={{
+              mt: 3,
+              mb: 2,
+              fontSize: 15,
+              backgroundColor: color,
+              "&:hover": {
+                backgroundColor: color,
+              },
+            }}
+            onClick={handleSubmit}
           >
             로그인 하기
           </Button>
@@ -53,6 +107,6 @@ const SignIn = () => {
       </Container>
     </>
   );
-}
+};
 
-export default SignIn
+export default SignIn;
--- client/src/components/Navigation/Navigation.jsx
+++ client/src/components/Navigation/Navigation.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
-const Navigation = (props) => {
+const Navigation = () => {
   return (
-    <div>
-      하단 탭 
-    </div>
-  )
-}
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Main</Link>
+        </li>
+        <li>
+          <Link to="/eatPayment">Eat Payment</Link>
+        </li>
+        <li>
+          <Link to="/pointpurchase">Point Purchase</Link>
+        </li>
+        <li>
+          <Link to="/productlist">Product List</Link>
+        </li>
+        <li>
+          <Link to="/userprofile">User Profile</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
-export default Navigation
+export default Navigation;
